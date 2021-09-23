af983ac8883b86096f1a5295be7fd2ca71f6ca62
Arwa Abdelrahem <arwaabdelrahem22@gmail.com>
2021-09-23 14:46:59 +0200

    requiring routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 require("./startup/DB");
+require("./startup/routes")(app);
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
